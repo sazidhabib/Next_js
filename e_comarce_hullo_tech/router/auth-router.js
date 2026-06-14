1903b7fb65ade41c6c8688b79066d2ae7079b10b
Mahbub Sazid Habib <68610034+sazidhabib@users.noreply.github.com>
2026-06-14 23:14:51 +0600

    impeccable skill added.

--- e_comarce_hullo_tech/router/auth-router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { loginUser, getProfile } = require('../controllers/auth-controller');
-const { protect } = require('../middlewares/auth-middleware');
-
-router.post('/login', loginUser);
-router.get('/profile', protect, getProfile);
-
-module.exports = router;
--- e_comarce_hullo_tech/router/setting-router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getSettings, updateSettings } = require('../controllers/setting-controller');
-const { protect, admin } = require('../middlewares/auth-middleware');
-
-router.get('/', getSettings);
-router.put('/', protect, admin, updateSettings);
-
-module.exports = router;
